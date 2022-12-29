180d721e389ebe9bcf6ac65db31b47448994e711
김용현 <i_am_gamer@naver.com>
2022-12-29 12:58:51 +0900

    <Practice>:<백준브5>
    고양이 어이없음

---
+++ 입출력과 사칙연산/10171.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cat
+{
+    class Program
+    {
+        static char[,] image = new char[4, 8];
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
