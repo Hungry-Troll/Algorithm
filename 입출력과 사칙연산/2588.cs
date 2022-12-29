85a98fb51f790ed659126f822af06cba60bddc48
Hungry-Troll <i_am_gamer@naver.com>
2022-12-30 06:54:46 +0900

    <Practice>:<백준 브5>
    별찍기 / 곱셈 / 이스케이프

--- 입출력과 사칙연산/2588.cs
+++ 입출력과 사칙연산/2588.cs
@@ -6,23 +6,17 @@ namespace Algorithm1
     {
         static void Main(string[] args)
         {
-            string s = Console.ReadLine();
-            string[] ss = s.Split();
-            int temp1 = int.Parse(ss[0]);
-            int temp2 = int.Parse(ss[1]);
-            int num1 = temp1 / 100;
-            int num2 = (temp1 % 100) / 10;
-            int num3 = (temp1 % 10);
-            int num4 = temp2 / 100;
-            int num5 = (temp2 % 100) / 10;
-            int num6 = (temp2 % 10);
-
-            Console.WriteLine((num1 * num6 * 100) + (num2 * num6 * 10) + (num3 * num6));
-            Console.WriteLine(((num1 * num5 * 100) + (num2 * num5 * 10) + (num3 * num5)));
-            Console.WriteLine(((num1 * num4 * 100) + (num2 * num4 * 10) + (num3 * num4)));
-            Console.WriteLine(((num1 * num6 * 100) + (num2 * num6 * 10) + (num3 * num6)) +
-                (((num1 * num5 * 100) + (num2 * num5 * 10) + (num3 * num5)) * 10) +
-                (((num1 * num4 * 100) + (num2 * num4 * 10) + (num3 * num4)) * 100));
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            int numA = int.Parse(s1);
+            int numB = int.Parse(s2);
+            int num100 = numB / 100;
+            int num10 = (numB / 10) % 10;
+            int num1 = (numB) % 10;
+            Console.WriteLine(numA * num1);
+            Console.WriteLine(numA * num10);
+            Console.WriteLine(numA * num100);
+            Console.WriteLine(numA * numB);
         }
     }
 
---
+++ 입출력과 사칙연산/Algorithm2/Algorithm2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algorithm2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            int num = int.Parse(s1);
+            int count = 0;
+            while(num > count)
+            {
+                count++;
+                Count(count);
+            }
+        }
+        static void Count(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
