a1127e22d70785785b563cb76864d0c2a0f9b289
김용현 <i_am_gamer@naver.com>
2022-12-29 10:19:18 +0900

    <Practice>:<백준 브5>
    사칙연산

---
+++ 입출력과 사칙연산/18108.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Algorithm1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int num1 = int.Parse(s);
+            Console.WriteLine(num1 - 543);
+        }
+    }
+
+}
---
+++ 입출력과 사칙연산/3003.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Algorithm1
+{
+    class Program
+    {
+        static int[] num = new int[6];
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            for (int i = 0; i < ss.Length; i++)
+            {
+                num[i] = int.Parse(ss[i]);
+            }
+
+            Console.WriteLine((1 - num[0]) + " " + (1 - num[1]) + " " + (2 - num[2]) + " " + (2 - num[3]) + " " +
+                (2 - num[4]) + " " + (8 - num[5]));
+        }
+    }
+}
