8b19e14072e3f8d3bcbd243dc820457984312ca5
김용현 <i_am_gamer@naver.com>
2023-01-02 01:10:23 +0900

    <Feat>:<백준 커밋 자동연결>
    자동으로 연결되게 설정되서 번거롭게 따로 올릴 필요가 없을 듯
    크롬 연결프로그램이므로 다른 pc에서 할 때 설치 필요함

--- Program.cs
+++ Program.cs
@@ -7,32 +7,8 @@ namespace Algorithm2
         static void Main(string[] args)
         {
             string s = Console.ReadLine();
-            int TestCount = int.Parse(s);
-            string ss = Console.ReadLine();
-            string [] TestPoint = ss.Split();
-            // 가장 큰 점수를 찾는다
-            int HighScore = 0;
-            // 어차피 찾으면서 배열에 점수를 넣기
-            int[] tmpPoint = new int[TestCount];
-            // 찾은 점수 계산용 배열
-            float[] SumPoint = new float[TestCount];
-            // 최종 함산 점수 변수
-            float SumScore = 0.0f;
-            for (int i = 0; i < TestCount; i++)
-            {
-                int tmp = int.Parse(TestPoint[i]);
-                tmpPoint[i] = tmp;
-                if (HighScore < tmp)
-                    HighScore = tmp;
-            }
-            // 점수 계산
-            for (int i = 0; i < TestCount; i++)
-            {
-                SumPoint [i] = (float)tmpPoint[i] / HighScore;
-                SumScore += SumPoint[i];
-            }
-            // 최종 계산
-            Console.WriteLine(SumScore / TestCount);
+            int length = s.Length;
+            Console.WriteLine(length);
         }
     }
 }
