d743a7d532e7f18e6fe5d3682502f400dd553cae
Kim youn Hyun <64481653+Hungry-Troll@users.noreply.github.com>
2023-01-08 18:35:42 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 68 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,12 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = 100;
+        for (int i = 0; i < count; i++)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+        }
+    }
+}
